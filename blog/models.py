f4bd5f0033f9acc693356a21ab9e844726dca01c
Tobias Brog <to.lokh@gmail.com>
2014-08-29 13:01:49 +0200

    New models for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    
+    
+class Entry(models.Model):
+    title  = models.CharField(max_length=50)
+    blog   = models.ForeignKey(Blog)
+    text   = models.CharField(max_length=4000)
+    date   = models-DateTimeField('date published')
+    author = models.ForeignKey(Author)
+
+
+
+
